a6c75e5a2e496e3c7831432fcd05d31dc644f47a
Keziyah <keziyahlewis@gmail.com>
2017-09-17 18:43:37 -0400

    changed js file a little

--- dist/assets/src/index.js
+++ dist/assets/src/index.js
@@ -1,4 +1,4 @@
-$( document ).ready( function() {
+$( window ).on('load', function() {
 
 //********** Header scroll stuff ****************//
 var didScroll;
@@ -34,53 +34,6 @@ function hasScrolled() {
     //********************************************** */
     //********************************************** */
     //********************************************** */
-
-	//Google Maps JS   https://codepen.io/hubpork/pen/xriIz 
-	//Set Map
-	function initialize() {
-			var myLatlng = new google.maps.LatLng(28.306171,-81.439076);
-			var imagePath = 'http://m.schuepfen.ch/icons/helveticons/black/60/Pin-location.png'
-			var mapOptions = {
-				zoom: 15,
-				center: myLatlng,
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-			}
-//
-		var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-		//Callout Content
-		var contentString = 
-            '<h3 id="firstHeading" class="firstHeading">Life Solutions Counseling</h3>'+
-            '<div id="bodyContent">'+
-            	'<p>We are located in La Mirada Plaza off Vine Street in Kissimmee, FL.</p>'+
-            '</div>';
-		//Set window width + content
-		var infowindow = new google.maps.InfoWindow({
-			content: contentString,
-			maxWidth: 300
-		});
-
-		//Add Marker
-		var marker = new google.maps.Marker({
-			position: myLatlng,
-			map: map,
-			// animation: google.maps.Animation.DROP,
-			icon: imagePath,
-			title: 'Life Solutions Counseling'
-		});
-
-		google.maps.event.addListener(marker, 'click', function() {
-			infowindow.open(map,marker);
-		});
-
-		//Resize Function
-		google.maps.event.addDomListener(window, "resize", function() {
-			var center = map.getCenter();
-			google.maps.event.trigger(map, "resize");
-			map.setCenter(center);
-		});
-	}
-
-	google.maps.event.addDomListener(window, 'load', initialize);
 
 	//********************************************** */
     //********************************************** */
@@ -151,3 +104,52 @@ $('a[href*="#"]')
 });
 
 });
+
+$(document).ready(function() {
+	//Google Maps JS   https://codepen.io/hubpork/pen/xriIz 
+	//Set Map
+	function initialize() {
+		var myLatlng = new google.maps.LatLng(28.306171,-81.439076);
+		var imagePath = 'http://m.schuepfen.ch/icons/helveticons/black/60/Pin-location.png'
+		var mapOptions = {
+			zoom: 15,
+			center: myLatlng,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		}
+//
+	var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+	//Callout Content
+	var contentString = 
+					'<h3 id="firstHeading" class="firstHeading">Life Solutions Counseling</h3>'+
+					'<div id="bodyContent">'+
+						'<p>We are located in La Mirada Plaza off Vine Street in Kissimmee, FL.</p>'+
+					'</div>';
+	//Set window width + content
+	var infowindow = new google.maps.InfoWindow({
+		content: contentString,
+		maxWidth: 300
+	});
+
+	//Add Marker
+	var marker = new google.maps.Marker({
+		position: myLatlng,
+		map: map,
+		// animation: google.maps.Animation.DROP,
+		icon: imagePath,
+		title: 'Life Solutions Counseling'
+	});
+
+	google.maps.event.addListener(marker, 'click', function() {
+		infowindow.open(map,marker);
+	});
+
+	//Resize Function
+	google.maps.event.addDomListener(window, "resize", function() {
+		var center = map.getCenter();
+		google.maps.event.trigger(map, "resize");
+		map.setCenter(center);
+	});
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+})
